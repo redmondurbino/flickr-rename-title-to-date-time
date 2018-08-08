d2e09088308407c5799409ba1e1c112fbf37b078
Redmond Urbino <redmondurbino+github@gmail.com>
2018-08-07 21:33:44 -0700

    we can get the date and time taken

--- rename_title_to_datetime.py
+++ rename_title_to_datetime.py
@@ -14,17 +14,24 @@ print api_secret
 flickr_user_id= os.environ.get('FLICKR_USER_ID','')
 flickr_set_to_rename= os.environ.get('FLICKR_SET_TO_RENAME','')
 
+flickr_set_to_rename ='72157659400135206'
+
 flickr = flickrapi.FlickrAPI(api_key, api_secret)
 flickr.authenticate_via_browser(perms='write')
 
 
-photos = flickr.photos.search(user_id= flickr_user_id, per_page='10')
-sets = flickr.photosets.getList(user_id= flickr_user_id)
+#search = flickr.photos.search(user_id= flickr_user_id, per_page='100', text='-20')
+#print "search=",xml.etree.ElementTree.tostring(search)
+
+#sets = flickr.photosets.getList(user_id= flickr_user_id)
 
 #print xml.etree.ElementTree.tostring(sets)
 
+count =0
 for photo in flickr.walk_set(flickr_set_to_rename):
-    print photo.get('title'), photo.get('id')
+    count += 1
+    print count, photo.get('title'), photo.get('id')
+    print "----- photo -----"
     print xml.etree.ElementTree.tostring(photo)
     photo_id = photo.get('id')
     print photo_id
@@ -32,12 +39,16 @@ for photo in flickr.walk_set(flickr_set_to_rename):
     print "----- info -----"
     print xml.etree.ElementTree.tostring(info)
     print "---- date taken --- "
-    print photo.tag
-    print photo.attrib
-    for child in photo.getchildren():
-        print child.tag, child.attrib
-    #print "----- info -----"
-    #exif = flickr.photos.getExif(photo_id=photo_id)
-    #print xml.etree.ElementTree.tostring(exif)
+
+    
+    date_taken = ""
+    for elem in info.iter('dates'):
+        if elem.attrib.has_key('taken'):
+            date_taken = elem.attrib['taken']
+            break
+        else:
+            print "Warning no date taken info for ",  xml.etree.ElementTree.tostring(elem)
+    print photo.get('title'), "date_taken= ",date_taken
+
     print "----- end -----"
 
