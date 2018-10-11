ad84cb68898e0e2c0a50e8f8a2d142f4ffc46e96
Redmond Urbino <redmondurbino+github@gmail.com>
2018-10-11 00:46:29 -0700

    proper parsing of arguments

--- print_set_ids.py
+++ print_set_ids.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 import flickrapi
 import xml
@@ -9,20 +10,17 @@ import webbrowser
 
 logging.basicConfig()
 
-api_key = unicode(os.environ.get('FLICKR_API_KEY',''))
-print "API_KEY=",api_key
-api_secret = unicode(os.environ.get('FLICKR_API_SECRET',''))
-print "API_SECRET=",api_secret
-flickr_user_id= os.environ.get('FLICKR_USER_ID','')
-print "FLICKR_USER_ID=",flickr_user_id
+default_api_key = unicode(os.environ.get('FLICKR_API_KEY',''))
+print "default API_KEY=",default_api_key
+default_api_secret = unicode(os.environ.get('FLICKR_API_SECRET',''))
+print "default API_SECRET=",default_api_secret
+default_flickr_user_id= os.environ.get('FLICKR_USER_ID','')
+print "default FLICKR_USER_ID=",default_flickr_user_id
 
 
-flickr = flickrapi.FlickrAPI(api_key, api_secret)
-flickr.authenticate_via_browser(perms='read')
-
-def print_set_ids(user_id=flickr_user_id):
+def print_set_ids(flickr,args):
 	"""Prints out the set id and the set title for the given flickr user"""
-	sets = flickr.photosets.getList(user_id= flickr_user_id)
+	sets = flickr.photosets.getList(user_id= args.flickr_user_id)
 	#print xml.etree.ElementTree.tostring(sets)
 	print "SET_ID            SET_TITLE"
 	for child in sets:
@@ -33,10 +31,18 @@ def print_set_ids(user_id=flickr_user_id):
 
 		
 def parse_args():
-	return None
+    """ Parse the arguments """
+    parser = argparse.ArgumentParser(description = "Print the set ids and titles of a flickr user.")
+    parser.add_argument("--api_key", default = default_api_key, help = "the flickr api key. https://www.flickr.com/services/api/misc.api_keys.html")
+    parser.add_argument("--api_secret", default = default_api_secret, help = "the flickr api secret. https://www.flickr.com/services/api/misc.api_keys.html")
+    parser.add_argument("--flickr_user_id", default = default_flickr_user_id, help = "your flickr user id. https://www.webpagefx.com/tools/idgettr/")
+    args = parser.parse_args()
+    return args
 	
 def main(args):
-	print_set_ids()
+	flickr = flickrapi.FlickrAPI(args.api_key, args.api_secret)
+	flickr.authenticate_via_browser(perms='read')
+	print_set_ids(flickr, args)
 
 if __name__ == '__main__':
 	args = parse_args()
